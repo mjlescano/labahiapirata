d7ca62271b2cb91917fb015b55850eeee62b1683
Matias Lescano <matiasj.lescano@gmail.com>
2014-07-01 19:55:39 -0300

    basic

---
+++ app.js
@@ -0,0 +1,10 @@
+var http = require('http')
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.end('Hola! :D\n')
+})
+
+server.listen(80)
+
+console.log("Server running at http://127.0.0.1:80/")
